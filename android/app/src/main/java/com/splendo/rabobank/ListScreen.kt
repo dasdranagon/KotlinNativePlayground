52c879cdee9f52ae85bd63d99ed8a4fddda7b91b
Dennis Skokov <dennis@splendo.com>
2019-03-12 10:38:27 +0100

    dummy content

--- android/app/src/main/java/com/splendo/rabobank/ListScreen.kt
+++ android/app/src/main/java/com/splendo/rabobank/ListScreen.kt
@@ -2,11 +2,15 @@ package com.splendo.rabobank
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.splendo.rabobank.dummy.DummyContent
 
-class List : AppCompatActivity() {
-
+class ListScreen : AppCompatActivity(), ListFragment.OnListFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented")
     }
 }

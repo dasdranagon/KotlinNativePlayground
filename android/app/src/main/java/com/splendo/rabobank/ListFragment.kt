4ad460f29b8214cffd6256e67856e99948103f40
Dennis Skokov <dennis@splendo.com>
2019-03-13 10:03:37 +0100

    fix issue number android title

--- android/app/src/main/java/com/splendo/rabobank/ListFragment.kt
+++ android/app/src/main/java/com/splendo/rabobank/ListFragment.kt
@@ -11,9 +11,9 @@ import presenter.ListDisplayItem
 class ListFragment() : Fragment() {
     private var listAdapter = ListRecyclerViewAdapter()
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         if (view is RecyclerView) {
